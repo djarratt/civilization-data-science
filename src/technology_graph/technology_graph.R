a09802c7a696f637041d1e7338cc335e938d9896
Daniel Jarratt <daniel.jarratt@infinitecampus.com>
2020-06-25 22:13:20 -0500

    more graph objects

--- src/technology_graph/technology_graph.R
+++ src/technology_graph/technology_graph.R
@@ -8,6 +8,8 @@ require(ggraph)
 
 technology = read_csv("../../data/raw/technology.csv")
 buildable = read_csv("../../data/raw/buildable.csv")
+effect = read_csv("../../data/raw/effect.csv")
+improvement = read_csv("../../data/raw/improvement.csv")
 technology_dependency = read_csv("../../data/raw/technologyDependency.csv")
 
 technology_graph = technology_dependency %>%
@@ -22,6 +24,39 @@ buildable_graph = buildable %>%
              by = c("dependsOnBuildableID" = "buildableID")) %>%
   select(to = name, from)
 
+unit_upgrade_graph = buildable %>%
+  filter(!is.na(upgradesToBuildableID)) %>%
+  inner_join(buildable %>% rename(to = name),
+             by = c("upgradesToBuildableID" = "buildableID")) %>%
+  select(from = name, to) %>%
+  bind_rows(
+    buildable %>%
+      filter(!is.na(ancientRuinsUpgradeToBuildableID)) %>%
+      inner_join(buildable %>% rename(to = name),
+                 by = c("ancientRuinsUpgradeToBuildableID" = "buildableID")) %>%
+      select(from = name, to)
+  )
+
+effect_graph = effect %>%
+  inner_join(buildable %>% rename(from = name),
+             by = c("buildableID" = "buildableID")) %>%
+  inner_join(buildable %>% rename(to = name),
+             by = c("givesFreeBuildableID" = "buildableID")) %>%
+  select(to, from)
+
+improvement_graph = improvement %>%
+  filter(!is.na(dependsOnTechnologyID), !is.na(dependsOnBuildableID)) %>%
+  inner_join(buildable %>% rename(from = name),
+             by = c("dependsOnBuildableID" = "buildableID")) %>%
+  select(to = name, from) %>%
+  bind_rows(
+    improvement %>%
+      filter(!is.na(dependsOnTechnologyID), !is.na(dependsOnBuildableID)) %>%
+      inner_join(technology %>% rename(from = name),
+                 by = c("dependsOnTechnologyID" = "technologyID")) %>%
+      select(to = name, from)
+  )
+
 technology_buildable_graph = buildable %>%
   filter(!is.na(dependsOnTechnologyID)) %>%
   inner_join(technology %>% rename(from = name),
@@ -30,7 +65,10 @@ technology_buildable_graph = buildable %>%
 
 graph_data = technology_graph %>%
   bind_rows(buildable_graph) %>%
-  bind_rows(technology_buildable_graph)
+  bind_rows(technology_buildable_graph) %>%
+  bind_rows(unit_upgrade_graph) %>%
+  bind_rows(improvement_graph) %>%
+  bind_rows(effect_graph)
 graph_data_dense = graph_data %>%
   select(from = to, to = from)  # we want direction of centrality importance
                                 # to flow backwards in time
@@ -48,8 +86,9 @@ eigen$y = rnorm(nrow(eigen))
 ggplot(eigen, aes(centrality, y, label = rownames(eigen))) +
   geom_point() + geom_text(angle = 45)
 
-ggraph(graph_tbl_with_metrics, layout = 'kk') +
+# igraph_layouts <- c('star', 'circle', 'gem', 'dh', 'graphopt', 'grid', 'mds', 'randomly', 'fr', 'kk', 'drl', 'lgl')
+ggraph(graph_tbl_with_metrics, layout = 'fr') +
   geom_edge_link() +
-  geom_node_text(aes(label = name, color = community_infomap, size = pagerank + 1)) +
+  geom_node_text(aes(label = name, color = community_infomap)) +
   theme_graph() +
   theme(legend.position = "none")
